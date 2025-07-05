6bc531c7ca6b9091a2c69723968f7b1e2f6cf7ba
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-07-05 12:03:36 +0600

    verifuy tocken fix

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -1,118 +1,126 @@
-// import { NextFunction, Request, Response } from "express";
-// import jwt, { JwtPayload } from "jsonwebtoken";
-// import config, { HttpStatus } from "../DefaultConfig/config";
-// import auth_model from "../apis/Auth/auth_model";
+import { PrismaClient } from '@prisma/client';
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config, { HttpStatus } from '../config/config';
 
-// interface DecodedToken extends JwtPayload {
-//   id?: string;
-// }
+const prisma = new PrismaClient();
 
-// const verifyToken = (
-//   allowedRoles: string[] = [],
-//   privet: boolean = true,
-//   type: string = config.TOKEN_NAME,
-//   fn?: (req: Request) => any,
-// ) => {
-//   return async (
-//     req: Request,
-//     res: Response,
-//     next: NextFunction,
-//   ): Promise<void> => {
-//     try {
-//       let tokenWithBearer = req.headers.authorization || req.cookies[type];
+interface DecodedToken extends JwtPayload {
+  id?: string;
+}
 
-//       if (!tokenWithBearer && !privet) {
-//         return next();
-//       }
+const verifyToken = (
+  allowedRoles: string[] = [],
+  privet: boolean = true,
+  type: string = config.TOKEN_NAME,
+  fn?: (req: Request) => any,
+) => {
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      let tokenWithBearer = req.headers.authorization || req.cookies[type];
 
-//       if (!tokenWithBearer) {
-//         res.status(403).send({ success: false, message: "Forbidden access" });
-//         return;
-//       }
+      if (!tokenWithBearer && !privet) {
+        return next();
+      }
 
-//       let token: string;
-//       if (tokenWithBearer.startsWith("Bearer ")) {
-//         token = tokenWithBearer.split(" ")[1];
-//       } else {
-//         token = tokenWithBearer;
-//       }
+      if (!tokenWithBearer) {
+        res.status(403).send({ success: false, message: "Forbidden access" });
+        return;
+      }
 
-//       jwt.verify(
-//         token,
-//         config.ACCESS_TOKEN_SECRET || "",
-//         async (err, decoded) => {
-//           if (err) {
-//             res
-//               .status(401)
-//               .send({ success: false, message: "Unauthorized access" });
-//             return;
-//           }
+      let token: string;
+      if (tokenWithBearer.startsWith("Bearer ")) {
+        token = tokenWithBearer.split(" ")[1];
+      } else {
+        token = tokenWithBearer;
+      }
 
-//           const decodedToken = decoded as DecodedToken;
+      jwt.verify(
+        token,
+        config.ACCESS_TOKEN_SECRET || "",
+        async (err, decoded) => {
+          if (err) {
+            res
+              .status(401)
+              .send({ success: false, message: "Unauthorized access" });
+            return;
+          }
 
-//           if (type == config.ACCESS_TOKEN_NAME) {
-//             const [user, extra] = await Promise.all([
-//               auth_model.findOne({
-//                 email: decodedToken?.email,
-//               }),
-//               fn ? fn(req) : {},
-//             ]);
-//             // console.log(user)
-//             if (user && user?.accessToken == token) {
-//               req.user = user;
-//               req.extra = extra;
-//               return next();
-//             } else {
-//               return res.status(HttpStatus.NOT_FOUND).send({
-//                 success: false,
-//                 message: "token missing or token has been expired",
-//               });
-//             }
-//           }
+          const decodedToken = decoded as DecodedToken;
 
-//           const [user, extra] = await Promise.all([
-//             auth_model.findById(decodedToken.id),
-//             fn ? fn(req) : {},
-//           ]);
+          if (type == config.ACCESS_TOKEN_NAME) {
+            const [user, extra] = await Promise.all([
+              prisma.user.findUnique({
+                where: {
+                  email: decodedToken?.email,
+                },
+              }),
+              fn ? fn(req) : {},
+            ]);
+            // console.log(user)
+            if (user && user?.accessToken == token) {
+              req.user = user;
+              req.extra = extra;
+              return next();
+            } else {
+              return res.status(HttpStatus.NOT_FOUND).send({
+                success: false,
+                message: "token missing or token has been expired",
+              });
+            }
+          }
 
-//           if (!user) {
-//             if (privet) {
-//               res
-//                 .status(404)
-//                 .send({ success: false, message: "User not found" });
-//               return;
-//             } else {
-//               return next();
-//             }
-//           }
+          const [user, extra] = await Promise.all([
+            prisma.user.findUnique({
+              where: {
+                id: decodedToken.id,
+              },
+            }),
+            fn ? fn(req) : {},
+          ]);
 
-//           if (user.block) {
-//             return res
-//               .status(401)
-//               .send({ success: false, message: "You are blocked by admin" });
-//           }
-//           if (!user.is_verified) {
-//             return res.status(401).send({
-//               success: false,
-//               message: "You please verify your email",
-//             });
-//           }
-//           if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
-//             res.status(403).send({
-//               success: false,
-//               message: "Access denied: insufficient permissions",
-//             });
-//             return;
-//           }
-//           req.user = user.toObject();
-//           req.extra = extra;
-//           next();
-//         },
-//       );
-//     } catch (error) {
-//       next(error); // Forward errors to the error-handling middleware
-//     }
-//   };
-// };
+          if (!user) {
+            if (privet) {
+              res
+                .status(404)
+                .send({ success: false, message: "User not found" });
+              return;
+            } else {
+              return next();
+            }
+          }
 
-// export default verifyToken;
+          if (user.block) {
+            return res
+              .status(401)
+              .send({ success: false, message: "You are blocked by admin" });
+          }
+          if (!user.is_verified) {
+            return res.status(401).send({
+              success: false,
+              message: "You please verify your email",
+            });
+          }
+          if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
+            res.status(403).send({
+              success: false,
+              message: "Access denied: insufficient permissions",
+            });
+            return;
+          }
+          req.user = user.toObject();
+          req.extra = extra;
+          next();
+        },
+      );
+    } catch (error) {
+      next(error); // Forward errors to the error-handling middleware
+    }
+  };
+};
+
+export default verifyToken;
